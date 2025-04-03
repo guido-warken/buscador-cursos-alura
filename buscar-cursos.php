59a0df4385338123859998ab8399909e92d46c27
guido <guido.warken@gmail.com>
2025-04-03 00:59:43 -0300

    Add course search functionality using web scraping
    Integrate Guzzle and Symfony components to implement a course scraper. Created a `Buscador` class to fetch and parse course data and a script `buscar-cursos.php` to display the retrieved course names. Updated `composer.json` to include necessary dependencies.

---
+++ buscar-cursos.php
@@ -0,0 +1,19 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Alura\BuscadorDeCursos\Buscador;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$cliente = new Client(['base_uri' => 'https://www.alura.com.br', 'verify' => false]);
+$crawler = new Crawler();
+
+$buscador = new Buscador($cliente, $crawler);
+
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
+
+foreach ($cursos as $curso) {
+    echo $curso . PHP_EOL;
+
+}
---
+++ src/Buscador.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Alura\BuscadorDeCursos;
+
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Buscador
+{
+
+
+    /**
+     * @var Client
+     */
+    private Client $cliente;
+    /**
+     * @var Crawler
+     */
+    private Crawler $crawler;
+
+    public function __construct(Client $cliente, Crawler $crawler)
+    {
+        $this->cliente = $cliente;
+        $this->crawler = $crawler;
+    }
+
+    public function buscar(string $url): array
+    {
+        $resposta = $this->cliente->request('GET', $url);
+
+        $html = $resposta->getBody()->getContents();
+        $this->crawler->addHtmlContent($html);
+
+        $cursos = $this->crawler->filter('span.card-curso__nome');
+
+        $cursos = [];
+        foreach ($cursos as $curso) {
+            $cursos[] = $curso->textContent;
+        }
+
+        return $cursos;
+    }
+}
